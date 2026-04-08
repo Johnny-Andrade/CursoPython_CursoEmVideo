dad720922b1086d40a44cd73e576ff8dbbfde523
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-04-08 15:34:44 -0300

    Aula 04 e 05 feitas

---
+++ Aula04/desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Dia do seu nascimento = ')
+mes = input('Nome do mês do seu nascimento = ')
+ano = input('Ano do seu nascimento = ')
+
+print('Você nasceu no dia', dia, 'de ', mes, 'do ano de', ano)
---
+++ Aula04/desafio03.py
@@ -0,0 +1,4 @@
+print('Iremos somar!')
+n1 = int(input('Insira o primeiro número: '))
+n2 = int(input('Insira o segundo número: '))
+print('A soma de ',n1, 'e',n2, 'é: ',n1+n2)
