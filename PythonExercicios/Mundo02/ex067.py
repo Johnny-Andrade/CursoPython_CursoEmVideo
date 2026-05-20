5f147c13cbf8fff9a424925358c43624d3037caa
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-05-20 14:57:34 -0300

    Corrigido, while substituido por for

--- PythonExercicios/Mundo02/ex067.py
+++ PythonExercicios/Mundo02/ex067.py
@@ -2,14 +2,12 @@ print('--'*20)
 print('Tabuada v3')
 print('Digite um número negativo para finalizar')
 while True:
-    cont = 1
     print('--'*20)
     n = int(input('Digite um número para a tabuada: '))
     print('--'*20)
     if n < 0:
         break
-    while cont <= 10:
-        print(f'{cont:2} x {n} = {cont*n}')
-        cont += 1
+    for c in range(1,11):
+        print(f'{c:2} x {n} = {c*n}')
 print('Obrigado por nos escolher!')
 print('--'*20)
