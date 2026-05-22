12843173341f2291afcb30069662584fdd99fd38
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-05-22 16:49:01 -0300

    Corrigido item barato, outras mudanças menores
    Anteriormente, um item com preço 0 não seria contado, agora é.
    while continuar not in 'SsNn' implementado

--- PythonExercicios/Mundo02/ex070.py
+++ PythonExercicios/Mundo02/ex070.py
@@ -1,11 +1,12 @@
 total = caros = 0
+barato = ''
 print('=='*20)
 print('LOJA CLAUDÃO BARATÃO')
 print('=='*20)
 while True:
     nome = str(input('\nDigite o nome do produto: ')).strip()
     preço = float(input('Digite o preço do produto: R$'))
-    if total == 0:
+    if barato == '':
         barato = nome
         prebarato = preço
     if preço > 1000:
@@ -15,10 +16,10 @@ while True:
         prebarato = preço
         barato = nome
     continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
-    while continuar != 'S' and continuar != 'N':
+    while continuar not in 'SsNn':
         continuar = str(input('\033[31m[ERRO]\033[m Deseja continuar? [S/N]: ')).strip().upper()[0]
     if continuar == 'N':
         break
-print(f'No total, você gastou R${total:.2f}.')
+print(f'\nNo total, você gastou R${total:.2f}.')
 print(f'Tivemos {caros} produtos que custaram mais que R$1000.00')
 print(f'Por fim, o produto mais barato foi {barato}, que custou R${prebarato:.2f}')
