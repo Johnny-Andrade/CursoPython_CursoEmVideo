757d290439f8b5626973dbfcfccf59386c59a167
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-05-28 17:54:57 -0300

    alterado nome de var, adicionado ifs

--- PythonExercicios/Mundo02/ex071.py
+++ PythonExercicios/Mundo02/ex071.py
@@ -1,7 +1,7 @@
 print('=='*20)
 print('Eletrônicos Danone')
 print('=='*20)
-cedula50 = cedula20 = cedula10 = cedula1 = 0
+cedula50 = cedula20 = cedula10 = cedula01 = 0
 valor = int(input('\nQual valor você quer sacar? R$'))
 while valor > 50:
     cedula50 += 1
@@ -13,13 +13,17 @@ while valor > 10:
     cedula10 += 1
     valor -= 10
 while valor >= 1:
-    cedula1 += 1
+    cedula01 += 1
     valor -= 1
     if valor == 0:
         break
 print('Tivemos um Total de:')
-print(f'{cedula50} cédulas de R$50.00')
-print(f'{cedula20} cédulas de R$20.00')
-print(f'{cedula10} cédulas de R$10.00')
-print(f'{cedula1} cédulas de R$1.00')
+if cedula50 > 0:
+    print(f'{cedula50} cédulas de R$50.00')
+if cedula20 > 0:
+    print(f'{cedula20} cédulas de R$20.00')
+if cedula10 > 0:
+    print(f'{cedula10} cédulas de R$10.00')
+if cedula01 > 0:
+    print(f'{cedula01} cédulas de R$1.00')
 print('Volte sempre!')
