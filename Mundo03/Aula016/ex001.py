bfd408ced16b9d8982b019db7e99dd39739bc850
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-02 15:48:52 -0300

    Aula016 finalizada

--- Mundo03/Aula016/ex001.py
+++ Mundo03/Aula016/ex001.py
@@ -2,6 +2,7 @@ lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
 # Tuplas são feitas com parênteses (até python 3.5), listas com colchetes, e dicionários com chaves
 '''lanche[2] = 'Refrigerante' # Tuplas são imutáveis!
 print(lanche)'''
-for comida in lanche:
+for comida in lanche: # for comida in range(0,len(lanche)) também serve!
     print(f'Acabei de comer {comida}')
 print(f'Nossa, comi muito! Foram {len(lanche)} coisas.')
+print(sorted(lanche)) #para ordenar transformou em lista
