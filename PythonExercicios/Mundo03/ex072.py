7a8d6262f32bd3069bf978e5552ece6f303de5fc
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-08 19:05:42 -0300

    ex072 corrigido

--- PythonExercicios/Mundo03/ex072.py
+++ PythonExercicios/Mundo03/ex072.py
@@ -1,8 +1,12 @@
 print('-='*15)
 print('{: ^30}'.format('Extensonador'))
 print('-='*15)
-tupla = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+tupla = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 
+         'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 
+         'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 
+         'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 
+         'Dezenove', 'Vinte')
 num = int(input('Digite um número de 0 a 20: '))
-if num < 0 or num > 20:
+while num < 0 or num > 20:
     num = int(input('\033[31m[ERRO]\033[m Digite um número de 0 a 20: '))
-print('A versão extensa desse número é: {}'.format(tupla[num]))
+print(f'A versão extensa desse número é: {tupla[num]}')
