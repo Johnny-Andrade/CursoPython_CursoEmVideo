a63ef73080572c5d9a59346764a04861d0d8e01a
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-09 15:20:10 -0300

    ex077 corrigido

--- PythonExercicios/Mundo03/ex077.py
+++ PythonExercicios/Mundo03/ex077.py
@@ -1,6 +1,6 @@
-palavras = ('Lembranças', 'Formula', 'Bolsa', 'Aperitivo', 'Comprimido', 'Adorno', 'Saudar', 'Primo', 'Lua', 'Refrescar', 'Curso', 'Video')
+palavras = ('Lembranças', 'Fórmula', 'Bolsa', 'Aperitivo', 'Comprimido', 'Adorno', 'Saudar', 'Primo', 'Lua', 'Refrescar', 'Curso', 'Vídeo')
 for item in palavras:
     print(f'\nNa palavra {item} temos ',end='')
     for letra in item:
-        if letra.lower() in 'aeiou':
-            print(f'{letra} ', end='')
+        if letra.lower() in 'aáãâeéêiíîoóôõuúû':
+            print(f'{letra.lower()} ', end='')
