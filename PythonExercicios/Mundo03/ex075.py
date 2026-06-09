3af04ec30bffc2ec8198f6a7ed6aa3ca4e24bb72
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-09 14:53:01 -0300

    ex075 corrigido e otimizado

--- PythonExercicios/Mundo03/ex075.py
+++ PythonExercicios/Mundo03/ex075.py
@@ -1,22 +1,19 @@
-tupla = (int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')))
+tupla = (int(input('Digite um número: ')), 
+         int(input('Digite um número: ')), 
+         int(input('Digite um número: ')), 
+         int(input('Digite um número: ')))
 pares = 0
-if tupla.count(9) > 0:
-    print('O número 9 apareceu {} vezes'.format(tupla.count(9)))
-else:
-    print('Não há número 9.')
-if tupla.count(3) > 0:
-    print('O número 3 apareceu primeiro na {}° posição.'.format(tupla.index(3)+1))
-else:
-    print('Não há número 3.')
+print(f'O número 9 apareceu {tupla.count(9)} vezes' if 9 in tupla 
+      else 'Não há número 9.')
+print(f'O número 3 apareceu primeiro na {tupla.index(3)+1}° posição.' if 3 in tupla 
+      else 'Não há número 3.')
 for num in tupla:
     if num % 2 == 0:
         pares += 1
-        numpares1 = (num, )
-        if pares == 1:
-            todospares = numpares1
-        else:
-            todospares = todospares + numpares1
 if pares > 0:
-    print(f'Existem {pares} números pares, sendo eles {todospares}')
+    print(f'A quantidade de números pares é {pares}. Sendo: ', end='')
+    for n in tupla:
+        if n % 2 == 0:
+            print(f'{n} ', end='')
 else:
     print('Não há números pares.')
