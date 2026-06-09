a691b45116326d03c7ab48aafe2718003aeb55a0
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-09 15:04:54 -0300

    ex076 corrigido

--- PythonExercicios/Mundo03/ex076.py
+++ PythonExercicios/Mundo03/ex076.py
@@ -1,11 +1,11 @@
-tupla = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
-print('-='*20)
-print('{: ^40}'.format('LISTAGEM DE PREÇOS'))
-print('-='*20)
+tupla = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 
+         'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 
+         'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+print('-='*15)
+print('{: ^30}'.format('LISTAGEM DE PREÇOS'))
+print('-='*15)
 for item in tupla:
     if tupla.index(item) % 2 == 0:
-        produto = item
+        print(f'{item:.<20}',end='')
     else:
-        preco = item
-    if tupla.index(item) % 2 != 0:
-        print('{:.<20}R$ {:>6.2f}'.format(produto, item))
+        print(f'R$ {item:>6.2f}')
