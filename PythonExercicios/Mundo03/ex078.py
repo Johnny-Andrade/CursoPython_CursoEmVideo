afeb0d80a688f50b5e1ba312ec30870de19df109
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-17 17:52:24 -0300

    ex078 e ex079 corrigidos

--- PythonExercicios/Mundo03/ex078.py
+++ PythonExercicios/Mundo03/ex078.py
@@ -3,16 +3,15 @@ for n in range(0,5):
     valores.append(int(input(f'Digite um número para a posição {n}: ')))
 maior = max(valores)
 menor = min(valores)
-posMaior = []
-posMenor = []
-anteMaior = anteMenor = 0
-for numMaior in range (0, valores.count(maior)):
-    posMaior.append(valores.index(maior, anteMaior+1))
-    anteMaior += valores.index(maior)
-for numMenor in range (0, valores.count(menor)):
-    posMenor.append(valores.index(menor, anteMenor+1))
-    anteMenor += valores.index(menor)
 print('-='*10)
 print(f'Você digitou os valores {valores}')
-print(f'O maior valor é {maior}, que está nas posições {posMaior}')
-print(f'O menor valor é {menor}, que está nas posições {posMenor}')
+print(f'O maior valor é {maior}, que está nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'{i}, ',end='')
+print('e só!')
+print(f'O menor valor é {menor}, que está nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'{i}, ',end='')
+print('e só!')
--- PythonExercicios/Mundo03/ex079.py
+++ PythonExercicios/Mundo03/ex079.py
@@ -1,4 +1,4 @@
-lista = []
+lista = list()
 while True:
     num = int(input('Digite um valor para a lista: '))
     if num not in lista:
@@ -7,9 +7,10 @@ while True:
     else:
         print('Valor duplicado. Não adicionado...')
     confirm = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
-    while confirm not in 'SN':
+    while confirm not in 'SsNn':
         confirm = str(input('\033[31m[ERRO]\033[m Deseja continuar? [S/N]: ')).strip().upper()[0]
-    if confirm == 'N':
+    if confirm in 'Nn':
         break
 print('-='*20)
-print(f'Os valores digitados, em ordem crescente, são: {sorted(lista)}')
+lista.sort()
+print(f'Os valores digitados, em ordem crescente, são: {lista}')
