06a08ffae750defd19ac9fbb7dc79788192d21dc
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-06-30 16:41:38 -0300

    melhorado ex086

--- PythonExercicios/Mundo03/ex086.py
+++ PythonExercicios/Mundo03/ex086.py
@@ -2,6 +2,7 @@ linha1 = []
 linha2 = []
 linha3 = []
 matriz = [linha1, linha2, linha3]
+c = 0
 for indice in range(0,3):
     linha1.append(int(input(f'Digite um valor para [0, {indice}]: ')))
 for indice in range(0,3):
@@ -9,7 +10,10 @@ for indice in range(0,3):
 for indice in range(0,3):
     linha3.append(int(input(f'Digite um valor para [2, {indice}]: ')))
 print('-='*20)
-print(f'''[ {matriz[0][0]} ] [ {matriz[0][1]} ] [ {matriz[0][2]} ]
 [ {matriz[1][0]} ] [ {matriz[1][1]} ] [ {matriz[1][2]} ]
-[ {matriz[2][0]} ] [ {matriz[2][1]} ] [ {matriz[2][2]} ]
-''')
+for indice, item in enumerate(matriz):
+    for valor in matriz[indice]:
+        print(f'[ {valor} ]', end=' ')
+        c += 1
+        if c % 3 == 0:
+            print()
