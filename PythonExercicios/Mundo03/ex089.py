4457dd2bb1e14ad797986905e22820a1613196ee
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-07-06 16:35:23 -0300

    ex089 corrigido

--- PythonExercicios/Mundo03/ex089.py
+++ PythonExercicios/Mundo03/ex089.py
@@ -1,39 +1,26 @@
-todos = list()
-nome = list()
-notas = list()
-Alunota = list()
+ficha = list()
 while True:
-    nomAl = str(input('Digite o nome do aluno: ')).strip()
-    nome.append(nomAl)
-    n1 = float(input('Digite a nota 1: '))
-    n2 = float(input('Digite a nota 2: '))
-    notas.append(n1)
-    notas.append(n2)
-    Alunota.append(nome[:])
-    nome.clear()
-    Alunota.append(notas[:])
-    notas.clear()
-    todos.append(Alunota[:])
-    Alunota.clear()
+    nome = str(input('Nome: ')).strip()
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
     conf = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
     while conf not in 'SN':
         conf = str(input('\033[31m[ERRO]\033[m Quer continuar? [S/N]: ')).strip().upper()[0]
     if conf == 'N':
         break
-print('-='*15)
-print(f'N° | {'NOME':<15} MÉDIA')
-print('--'*15)
-for indice, valor in enumerate(todos):
-    media = ((todos[indice][1][0]+todos[indice][1][1])/2)
-    print(f'{indice+1:<3}| {todos[indice][0][0]:<15} {media:>4.1f}')
-print('--'*15)
+print('-='*30)
+print(f'{'N°':<4}{'Nome':<10}{'Média':>8}')
+print('--'*13)
+for indice, aluno in enumerate(ficha):
+    print(f'{indice+1:<4}{aluno[0]:<10}{aluno[2]:>8.1f}')
 while True:
-    escolha = int(input('Mostrar notas de qual aluno? [999 Finaliza]: '))
-    while ((escolha > len(todos)) and (escolha != 999)):
-        escolha = int(input('\033[31m[ERRO]\033[m Mostrar notas de qual aluno? [999 Finaliza]: '))
-    if escolha == 999:
+    print('-'*35)
+    opc = int(input('Quer ver as notas de qual aluno? [999 Finaliza]: '))
+    if opc == 999:
         break
-    else:
-        print(f'Notas de {todos[escolha-1][0][0]} são {todos[escolha-1][1]}')
-print('FINALIZANDO...\n')
+    if opc <= len(ficha):
+        print(f'Notas de {ficha[opc-1][0]} são {ficha[opc-1][1]}')
+print('\nFINALIZANDO...\n')
 print('<<< VOLTE SEMPRE >>>')
