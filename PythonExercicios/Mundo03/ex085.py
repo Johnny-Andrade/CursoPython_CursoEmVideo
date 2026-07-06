5d613408ac4580f80df91509f8be9d84c65e9667
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-07-06 14:59:29 -0300

    ex085 corrigido

--- PythonExercicios/Mundo03/ex085.py
+++ PythonExercicios/Mundo03/ex085.py
@@ -1,14 +1,12 @@
-pares = list()
-impares = list()
-numeros = [pares, impares]
-for n in range (1,8):
-    num = int(input(f'Digite o {n}° número: '))
+numeros = [[], []]
+for c in range (1,8):
+    num = int(input(f'Digite o {c}° número: '))
     if num % 2 == 0:
-        pares.append(num)
+        numeros[0].append(num)
     else:
-        impares.append(num)
-pares.sort()
-impares.sort()
+        numeros[1].append(num)
+numeros[0].sort()
+numeros[1].sort()
 print(f'Os números pares em ordem crescentes, são: {numeros[0]}')
 print(f'Os números ímpares em ordem crescente são: {numeros[1]}')
-print(f'Por fim, a lista completa é: {numeros}')
+print(f'\nPor fim, a lista completa é: {numeros}')
