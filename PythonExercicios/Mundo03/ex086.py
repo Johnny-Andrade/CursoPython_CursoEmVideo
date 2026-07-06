941b781d0847773e9d94eeb1cf86cc6789a6fd4c
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-07-06 15:08:42 -0300

    ex086 corrigido

--- PythonExercicios/Mundo03/ex086.py
+++ PythonExercicios/Mundo03/ex086.py
@@ -1,18 +1,9 @@
-linha1 = []
-linha2 = []
-linha3 = []
-matriz = [linha1, linha2, linha3]
-c = 0
-for indice in range(0,3):
-    linha1.append(int(input(f'Digite um valor para [0, {indice}]: ')))
-for indice in range(0,3):
-    linha2.append(int(input(f'Digite um valor para [1, {indice}]: ')))
-for indice in range(0,3):
-    linha3.append(int(input(f'Digite um valor para [2, {indice}]: ')))
+matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para a posição [{l}, {c}]:'))
 print('-='*20)
-for indice, item in enumerate(matriz):
-    for valor in matriz[indice]:
-        print(f'[ {valor} ]', end=' ')
-        c += 1
-        if c % 3 == 0:
-            print()
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
