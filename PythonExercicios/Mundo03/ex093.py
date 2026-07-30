55422e9b34d184ff09f005e60a942361c4f323d5
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-07-30 16:41:52 -0300

    ex093 corrigido

--- PythonExercicios/Mundo03/ex093.py
+++ PythonExercicios/Mundo03/ex093.py
@@ -1,19 +1,18 @@
-nome = str(input('Diga o nome do jogador: ')).strip()
+dados = dict()
+dados['Nome'] = str(input('Diga o nome do jogador: ')).strip()
 npart = int(input('Quantas partidas ele jogou? '))
 gols = list()
-totgol = ngol = 0
 for p in range(0, npart):
-    ngol = int(input(f'Quantos gols {nome} fez no {p+1}° jogo? '))
-    gols.append(ngol)
-    totgol += ngol
+    gols.append(int(input(f'Quantos gols {dados["Nome"]} fez no {p+1}° jogo? ')))
 print('--'*20)
-dados = {'Nome': nome, 'NGols': gols, 'Total': totgol}
+dados['NGols'] = gols
+dados['Total'] = sum(gols)
 print(dados)
 print('--'*20)
 for key, value in dados.items():
     print(f'O campo {key} tem valor {value}')
 print('-='*20)
-print(f'O jogador {nome} jogou {npart} partidas.')
-for indice, item in enumerate(gols):
+print(f'O jogador {dados['Nome']} jogou {npart} partidas.')
+for indice, item in enumerate(dados['NGols']):
     print(f'    => Na partida {indice+1}, fez {item} gols.')
-print(f'Fez um total de {totgol} gols.')
+print(f'Fez um total de {dados['Total']} gols.')
