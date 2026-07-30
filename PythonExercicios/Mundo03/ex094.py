5625d51a746ec32e0d4339d6a0d81c24ad1e1b4e
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-07-30 16:53:01 -0300

    ex094 feito

--- PythonExercicios/Mundo03/ex094.py
+++ PythonExercicios/Mundo03/ex094.py
@@ -1,25 +1,30 @@
-totPes = idadeMed = 0
+idadeMed = 0
 mulheres = list()
+pessoa = dict()
+todos = list()
 while True:
-    totPes += 1 
-    nome = str(input('Digite o nome: ')).strip()
-    sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    while sexo not in 'FM':
-        sexo = str(input('\033[31m[ERRO]\033[m Sexo: [M/F] ')).strip().upper()[0]
-    if sexo == 'F':
-        mulheres.append(nome)
-    idade = int(input('Digite a idade: '))
-    idadeMed += idade
+    pessoa["Nome"] = str(input('Digite o nome: ')).strip()
+    pessoa["Sexo"] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    while pessoa["Sexo"] not in 'FM':
+        pessoa["Sexo"] = str(input('\033[31m[ERRO]\033[m Sexo: [M/F] ')).strip().upper()[0]
+    if pessoa["Sexo"] == 'F':
+        mulheres.append(pessoa["Nome"])
+    pessoa['Idade'] = int(input('Digite a idade: '))
+    idadeMed += pessoa['Idade']
     conf = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     while conf not in 'SN':
         conf = str(input('\033[31m[ERRO]\033[m Deseja continuar? [S/N] ')).strip().upper()[0]
+    todos.append(pessoa.copy())
     if conf == 'N':
-        idadeMed /= totPes
+        idadeMed /= len(todos)
         break
 print('-='*20)
-print(f'- O grupo tem {totPes} pessoas.')
-print(f'- A média de idade é de {idadeMed} anos.')
-print(f'As mulheres cadastradas foram: ', end='')
+print(f'A) O grupo tem {len(todos)} pessoas.')
+print(f'B) A média de idade é de {idadeMed} anos.')
+print(f'C) As mulheres cadastradas foram: ', end='')
 for item in mulheres:
     print(f'{item} ', end='')
-print()
+print(f'D) Lista das pessoas com idade acima da média: ')
+for item in todos:
+    if item["Idade"] > idadeMed:
+        print(item)
